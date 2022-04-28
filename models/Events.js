3496bd72160a415b26594de727e27fba84d69bf4
HZW211 <zhiweihu@brandeis.edu>
2022-04-27 21:02:57 -0400

    delete some files and methods

--- models/Events.js
+++ models/Events.js
@@ -5,25 +5,8 @@ const ObjectId = Schema.Types.ObjectId;
 const Mixed = Schema.Types.Mixed;
 
 var eventSchema = Schema( {
-    // limit: Number,
-    // times: Mixed,
-    // enrolled: Number,
     day: String,
     thisevent: String,
-    // status_text: String,
-    // section: String,
-    // waiting: Number,
-    // instructor: Mixed,
-    // coinstructors: Mixed,
-    // code: Mixed,
-    // subject: String,
-    // coursenum: String,
-    // num: Number,
-    // suffix: String,
-    // name: String,
-    // independent_study: Boolean,
-    // term: Number,
-    // description: String,
 } );
 
 module.exports = mongoose.model( 'Events', eventSchema );

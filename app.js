7365eedde62b19413bdb1695ad6d8d7b21e8a831
HZW211 <zhiweihu@brandeis.edu>
2022-04-27 21:12:52 -0400

    final push

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const events = require('./public/data/events.json')
 
 const mongoose = require( 'mongoose' );
 
-//const mongodb_URI = process.env.mongodb_URI //looking at environment variables
+// const mongodb_URI = process.env.mongodb_URI //looking at environment variables
 //const mongodb_URI = 'mongodb://localhost:27017/cs103a_todo'
 // const mongodb_URI = 'mongodb+srv://cs_sj:BrandeisSpr22@cluster0.kgugl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const mongodb_URI = 'mongodb+srv://ZhiweiHu:From2018@cluster0.vdt2c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -183,55 +183,8 @@ app.get('/todo',
 )
 
 /* ************************
-  Functions needed for the course finder routes
+  Functions needed for routes
    ************************ */
-
-// function getNum(coursenum){
-//   // separate out a coursenum 103A into 
-//   // a num: 103 and a suffix: A
-//   i=0;
-//   while (i<coursenum.length && '0'<=coursenum[i] && coursenum[i]<='9'){
-//     i=i+1;
-//   }
-//   return coursenum.slice(0,i);
-// }
-
-
-// function times2str(times){
-//   // convert a course.times object into a list of strings
-//   // e.g ["Lecture:Mon,Wed 10:00-10:50","Recitation: Thu 5:00-6:30"]
-//   if (!times || times.length==0){
-//     return ["not scheduled"]
-//   } else {
-//     return times.map(x => time2str(x))
-//   }
-  
-// }
-// function min2HourMin(m){
-//   // converts minutes since midnight into a time string, e.g.
-//   // 605 ==> "10:05"  as 10:00 is 60*10=600 minutes after midnight
-//   const hour = Math.floor(m/60);
-//   const min = m%60;
-//   if (min<10){
-//     return `${hour}:0${min}`;
-//   }else{
-//     return `${hour}:${min}`;
-//   }
-// }
-
-// function time2str(time){
-//   // creates a Times string for a lecture or recitation, e.g. 
-//   //     "Recitation: Thu 5:00-6:30"
-//   const start = time.start
-//   const end = time.end
-//   const days = time.days
-//   const meetingType = time['type'] || "Lecture"
-//   const location = time['building'] || ""
-
-//   return `${meetingType}: ${days.join(",")}: ${min2HourMin(start)}-${min2HourMin(end)} ${location}`
-// }
-
-
 
 /* ************************
   Loading (or reloading) the data into a collection
@@ -259,19 +212,6 @@ app.get('/upsertDB',
   }
 )
 
-
-// app.post('/courses/bySubject',
-//   // show list of courses in a given subject
-//   async (req,res,next) => {
-//     const {subject} = req.body;
-//     const courses = await Course.find({subject:subject,independent_study:false}).sort({term:1,num:1,section:1})
-    
-//     res.locals.courses = courses
-//     res.locals.times2str = times2str
-//     //res.json(courses)
-//     res.render('courselist')
-//   }
-// )
 app.get('/events/show',
   (req,res,next) => {
     res.render('schedule')
@@ -307,96 +247,7 @@ app.post('/events/delete',
   }
 )
 
-// app.get('/courses/show/:courseId',
-//   // show all info about a course given its courseid
-//   async (req,res,next) => {
-//     const {courseId} = req.params;
-//     const course = await Course.findOne({_id:courseId})
-//     res.locals.course = course
-//     res.locals.times2str = times2str
-//     //res.json(course)
-//     res.render('course')
-//   }
-// )
-
-// app.get('/courses/byInst/:email',
-//   // show a list of all courses taught by a given faculty
-//   async (req,res,next) => {
-//     const email = req.params.email+"@brandeis.edu";
-//     const courses = await Course.find({instructor:email,independent_study:false})
-//     //res.json(courses)
-//     res.locals.courses = courses
-//     res.render('courselist')
-//   } 
-// )
-
-// app.post('/courses/byInst',
-//   // show courses taught by a faculty send from a form
-//   async (req,res,next) => {
-//     const email = req.body.email+"@brandeis.edu";
-//     const courses = 
-//        await Course
-//                .find({instructor:email,independent_study:false})
-//                .sort({term:1,num:1,section:1})
-//     //res.json(courses)
-//     res.locals.courses = courses
-//     res.locals.times2str = times2str
-//     res.render('courselist')
-//   }
-// )
-
 app.use(isLoggedIn)
-
-// app.get('/addCourse/:courseId',
-//   // add a course to the user's schedule
-//   async (req,res,next) => {
-//     try {
-//       const courseId = req.params.courseId
-//       const userId = res.locals.user._id
-//       // check to make sure it's not already loaded
-//       const lookup = await Schedule.find({courseId,userId})
-//       if (lookup.length==0){
-//         const schedule = new Schedule({courseId,userId})
-//         await schedule.save()
-//       }
-//       res.redirect('/schedule/show')
-//     } catch(e){
-//       next(e)
-//     }
-//   })
-
-// app.get('/schedule/show',
-//   // show the current user's schedule
-//   async (req,res,next) => {
-//     try{
-//       const userId = res.locals.user._id;
-//       const courseIds = 
-//          (await Schedule.find({userId}))
-//                         .sort(x => x.term)
-//                         .map(x => x.courseId)
-//       res.locals.courses = await Course.find({_id:{$in: courseIds}})
-//       res.render('schedule')
-//     } catch(e){
-//       next(e)
-//     }
-//   }
-// )
-
-// app.get('/schedule/remove/:courseId',
-//   // remove a course from the user's schedule
-//   async (req,res,next) => {
-//     try {
-//       await Schedule.remove(
-//                 {userId:res.locals.user._id,
-//                  courseId:req.params.courseId})
-//       res.redirect('/schedule/show')
-
-//     } catch(e){
-//       next(e)
-//     }
-//   }
-// )
-
 
 // here we catch 404 errors and forward to error handler
 app.use(function(req, res, next) {
